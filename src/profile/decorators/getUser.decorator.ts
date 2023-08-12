8d1355ffd56b136a6dbd9eadb07de9af7fe30fd4
TheophilusBoakye <theophilus.yiadom@amalitech.com>
2023-08-12 21:32:18 +0000

    create custom decorator

---
+++ src/profile/decorators/getUser.decorator.ts
@@ -0,0 +1,10 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+import { Request } from 'express';
+
+export const GetUser = createParamDecorator(
+  (data: string | undefined, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest<Request>();
+    const user = request.user;
+    return data ? user[data] : user;
+  },
+);
